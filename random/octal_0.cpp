3f321d0e9c044efb12bb229f9fcf4895d49ac4ed
RitikaDadwal <ritikadadwal21@gmail.com>
2024-01-18 10:36:02 +0530

    Update code

--- random/octal_0.cpp
+++ random/octal_0.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
     int number1 = 23;
     int number2 = 0+23;
-    int number3 = 023;
+    int number3 = 023; //considers the number as an octal number.
     cout<<"The number1 is : "<<number1<<endl;
     cout<<"The number2 is : "<<number2<<endl;
     cout<<"The number3 is : "<<number3<<endl;
